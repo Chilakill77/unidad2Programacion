e75df775afe9635bfb80c88bbeb30e51845f28e3
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-23 11:31:24 -0600

    Add files via upload

---
+++ parse_json04.py
@@ -0,0 +1,44 @@
+#Cabrera Luna Kevin
+import urllib.parse
+import requests
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Dolores Hidalgo"
+dest = "San Miguel de Allende"
+key = "kJI2zNJ9v75CAlaxsEnXHkyutNojJ0tN"
+#Extrae la ruta y la ID de sesion
+url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+json_data = requests.get(url).json()
+print (json_data ['route'] ['sessionId'])
+#Extrae la distancia y el tiempo
+distance= (json_data ['route']['distance'])
+time = (json_data['route']['time'])
+print (distance)
+print (time)
+#Extrae el primer elemento legs
+formattedTime= (json_data ['route']['legs']['formattedTime'])
+print (formattedTime)
+#Paso DOS
+print("URL: " + (url))
+
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
+while True:
+    orig = input("Starting Location: ")
+    dest = input("Destination: ")
+    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+    print("URL: " + (url))
+#Imprimir Url
+print("URL: " + (url))
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
+#Latitud y Longitud
+json_data = requests.get(url).json
+lat = (json_data['route']['lat'])
+
+# paso 5
